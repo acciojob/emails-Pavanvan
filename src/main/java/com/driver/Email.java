d3e424bcab0ef2b1ec2c261a26af24d732bb02dc
pavanvan <pavangullapalli09@gmail.com>
2022-12-24 18:40:25 +0530

    Code

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,30 +1,29 @@
 package com.driver;
 
-import java.util.regex.Pattern;
-
 public class Email {
+
     private String emailId;
     private String password;
 
-    public Email(String emailId) {
+    public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
     }
 
-    public void changePassword(String oldPassword, String newPassword) {
-        if (!oldPassword.equals(this.password)) {
-            return;
-        }
+    public String getEmailId() {
+        return emailId;
+    }
 
-        // check if newPassword meets all the required conditions
-        if (!Pattern.matches(".*[A-Z].*", newPassword) ||
-                !Pattern.matches(".*[a-z].*", newPassword) ||
-                !Pattern.matches(".*[0-9].*", newPassword) ||
-                !Pattern.matches(".*[^A-Za-z0-9].*", newPassword) ||
-                newPassword.length() < 8) {
-            return;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        this.password = newPassword;
+    public void changePassword(String oldPassword, String newPassword){
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
     }
 }
--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -6,16 +6,9 @@ public class Meeting {
     private LocalTime startTime;
     private LocalTime endTime;
 
-    public Meeting(LocalTime startTime, LocalTime endTime) {
+    public Meeting(LocalTime startTime, LocalTime endTime){
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
 }
